7e4b2c6c7d2cc20b2a64095c80c4832d78f04efd
bperraud <74benji@live.fr>
2023-04-28 15:01:52 +0200

    redirect url with callback

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { OAuthController } from './oauth.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, OAuthController],
   providers: [AppService],
 })
 export class AppModule {}
