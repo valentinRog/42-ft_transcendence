8d499227b61f3db15ef195f8ba97cad437714d0f
bperraud <74benji@live.fr>
2023-05-05 14:56:19 +0200

    merge

---
+++ backend/prisma/migrations/20230504133218_new/migration.sql
@@ -0,0 +1,38 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "login" TEXT NOT NULL,
+    "username" TEXT NOT NULL DEFAULT '',
+    "phone" TEXT,
+    "image" TEXT,
+    "status" TEXT NOT NULL DEFAULT 'active',
+    "twoFactorEnabled" BOOLEAN NOT NULL DEFAULT false,
+    "friends" INTEGER[] DEFAULT ARRAY[]::INTEGER[],
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Stat" (
+    "id" SERIAL NOT NULL,
+    "wins" INTEGER NOT NULL DEFAULT 0,
+    "losses" INTEGER NOT NULL DEFAULT 0,
+    "elo" INTEGER NOT NULL DEFAULT 1000,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "Stat_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_login_key" ON "User"("login");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Stat_userId_key" ON "Stat"("userId");
+
+-- AddForeignKey
+ALTER TABLE "Stat" ADD CONSTRAINT "Stat_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
