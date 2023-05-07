4f7f0f7dd518740f2871e2ad2cf710d07a0d0fd6
bperraud <74benji@live.fr>
2023-05-07 21:06:45 +0200

    permissions

--- backend/test/app.e2e-spec.ts
+++ backend/test/app.e2e-spec.ts
@@ -1,24 +1,149 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import {
+	INestApplication,
+	ValidationPipe,
+  } from '@nestjs/common';
+  import { Test } from '@nestjs/testing';
+  import * as pactum from 'pactum';
+  import { AppModule } from '../src/app.module';
+  import { AuthDto } from '../src/auth/dto';
 
-describe('AppController (e2e)', () => {
-  let app: INestApplication;
+  import { PrismaService } from '../src/prisma/prisma.service';
+  import { EditUserDto } from '../src/user/dto';
 
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
+  describe('App e2e', () => {
+	let app: INestApplication;
+	let prisma: PrismaService;
 
-    app = moduleFixture.createNestApplication();
-    await app.init();
+	beforeAll(async () => {
+	  const moduleRef =
+		await Test.createTestingModule({
+		  imports: [AppModule],
+		}).compile();
+
+	  app = moduleRef.createNestApplication();
+	  app.useGlobalPipes(
+		new ValidationPipe({
+		  whitelist: true,
+		}),
+	  );
+	  await app.init();
+	  await app.listen(3333);
+
+	  prisma = app.get(PrismaService);
+	  await prisma.cleanDb();
+	  pactum.request.setBaseUrl('http://localhost:3333');
+	});
+
+	afterAll(() => {
+	  app.close();
+	});
+
+	describe('Auth', () => {
+	  const dto: AuthDto = {
+		login: 'vlad',
+		password: '123',
+	  };
+	  describe('Signup', () => {
+		it('should throw if login empty', () => {
+		  return pactum
+			.spec()
+			.post('/auth/signup')
+			.withBody({
+			  password: dto.password,
+			})
+			.expectStatus(400);
+		});
+		it('should throw if password empty', () => {
+		  return pactum
+			.spec()
+			.post('/auth/signup')
+			.withBody({
+			  login: dto.login,
+			})
+			.expectStatus(400);
+		});
+		it('should throw if no body provided', () => {
+		  return pactum
+			.spec()
+			.post('/auth/signup')
+			.expectStatus(400);
+		});
+		it('should signup', () => {
+		  return pactum
+			.spec()
+			.post('/auth/signup')
+			.withBody(dto)
+			.expectStatus(201);
+		});
+	  });
+
+	  describe('Signin', () => {
+		it('should throw if login empty', () => {
+		  return pactum
+			.spec()
+			.post('/auth/signin')
+			.withBody({
+			  password: dto.password,
+			})
+			.expectStatus(400);
+		});
+		it('should throw if password empty', () => {
+		  return pactum
+			.spec()
+			.post('/auth/signin')
+			.withBody({
+			  login: dto.login,
+			})
+			.expectStatus(400);
+		});
+		it('should throw if no body provided', () => {
+		  return pactum
+			.spec()
+			.post('/auth/signin')
+			.expectStatus(400);
+		});
+		it('should signin', () => {
+		  return pactum
+			.spec()
+			.post('/auth/signin')
+			.withBody(dto)
+			.expectStatus(200)
+			.stores('userAt', 'access_token');
+		});
+	  });
+	});
+
+	describe('User', () => {
+	  describe('Get me', () => {
+		it('should get current user', () => {
+		  return pactum
+			.spec()
+			.get('/users/me')
+			.withHeaders({
+			  Authorization: 'Bearer $S{userAt}',
+			})
+			.expectStatus(200);
+		});
+	  });
+
+	  describe('Edit user', () => {
+		it('should edit user', () => {
+		  const dto: EditUserDto = {
+			firstName: 'Vladimir',
+			login: 'vlad@codewithvlad.com',
+		  };
+		  return pactum
+			.spec()
+			.patch('/users')
+			.withHeaders({
+			  Authorization: 'Bearer $S{userAt}',
+			})
+			.withBody(dto)
+			.expectStatus(200)
+			.expectBodyContains(dto.firstName)
+			.expectBodyContains(dto.login);
+		});
+	  });
+
+	});
   });
-
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
-  });
-});
