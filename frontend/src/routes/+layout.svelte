da2d096f7d2d2a8f5fecd2a4d949c07d0066e1e3
Louise Rondia <louiserondia@gmail.com>
2023-05-10 17:39:00 +0200

    :sparkles: setup style & svelte

---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+    import '$lib/style/style.scss'
+</script>
+
+<slot></slot>
---
+++ frontend/src/routes/lrondia/+page.svelte
@@ -0,0 +1,15 @@
+<h1>louise</h1>
+
+<style lang="scss">
+
+@import '$lib/style/var';
+
+:global(body) {
+    background-color: $background;
+}
+
+h1 {
+    color: rgb(19, 41, 49);
+}
+
+</style>
