7ee834ddde818d930b317e1462ad55e1a5972d62
bperraud <74benji@live.fr>
2023-05-11 16:55:05 +0200

    upload from file

--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -2,25 +2,9 @@ import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { HttpModule } from '@nestjs/axios';
-import { MulterModule } from '@nestjs/platform-express';
-import { diskStorage } from 'multer';
-import UPLOAD_PATH from '../../config/upload-path';
 
 @Module({
-  imports: [
-    //MulterModule.register({
-    //  storage: diskStorage({
-    //    destination: UPLOAD_PATH,
-    //    filename: (req, file, cb) => {
-    //      //const user = req.user;
-    //      console.log(req.user);
-    //      const fileName = 'module' + '.png';
-    //      cb(null, fileName);
-    //    },
-    //  }),
-    //}),
-    HttpModule,
-  ],
+  imports: [HttpModule],
   controllers: [UserController],
   providers: [UserService],
 })
