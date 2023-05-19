80b198603ed91ce73ea201d1895d8db548fbba4f
bperraud <74benji@live.fr>
2023-05-19 02:12:16 +0200

    no event

--- backend/src/matchmaking/matchmaking.service.ts
+++ backend/src/matchmaking/matchmaking.service.ts
@@ -1,46 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
-import { EventEmitter2 } from '@nestjs/event-emitter';
 import { PlayerDto } from './dto';
-import {
-  PlayerJoinedQueueEvent,
-  MatchFoundEvent,
-  PlayerLeftQueueEvent,
-} from './../events/matchmaking.events';
 
 @Injectable()
 export class MatchmakingService {
-  constructor(
-    private userService: UserService,
-    private eventEmitter: EventEmitter2,
-  ) {}
+  constructor(private userService: UserService) {}
 
   async handlePlayerJoinedQueue(player: PlayerDto) {
     // Add player to the matchmaking queue logic
-
-    // Emit PlayerJoinedQueueEvent
-    const playerJoinedQueueEvent = new PlayerJoinedQueueEvent(player);
-    this.eventEmitter.emit('playerJoinedQueue', playerJoinedQueueEvent);
   }
 
   // Other matchmaking methods...
 
   async handlePlayerLeftQueue(player: PlayerDto): Promise<void> {
     // Remove player from the matchmaking queue logic
-
-    // Emit PlayerLeftQueueEvent
-    const playerLeftQueueEvent = new PlayerLeftQueueEvent(player);
-    this.eventEmitter.emit('playerLeftQueue', playerLeftQueueEvent);
   }
 
   // Matchmaking logic...
 
   async handleMatchFound(players: PlayerDto[]): Promise<void> {
     // Handle the matched players logic
-
-    // Emit MatchFoundEvent
-    const matchFoundEvent = new MatchFoundEvent(players);
-    this.eventEmitter.emit('matchFound', matchFoundEvent);
   }
 
   async queue(userId: number) {
