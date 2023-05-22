246fb8a061b550b67eaa86e6cd5b74038300ae8d
Louise Rondia <louiserondia@gmail.com>
2023-05-22 18:03:41 +0200

    refactoring

--- frontend/src/lib/components/Tab.svelte
+++ frontend/src/lib/components/Tab.svelte
@@ -13,28 +13,15 @@
 
 <style lang="scss">
 	.tab {
-		// @include tab;
+		@include tab;
 		
-		@include tab-contour;
-		@include tab-contour-active;
-		img {
-			@include tab-logo;
-		}
-		
-		background-color: $grey;
-		height: $tab-height;
 		display: flex;
-		padding: 0.2rem 0.8rem;
-		margin-right: 0.25rem;
 		flex-basis: 15rem;
 		flex-shrink: 1;
 		flex-wrap: nowrap;
 		overflow: hidden;
 
 		a {
-			font-size: 1.2rem;
-			text-decoration: none;
-			color: inherit;
 			white-space: nowrap;
 			overflow: hidden;
 			text-overflow: ellipsis;
