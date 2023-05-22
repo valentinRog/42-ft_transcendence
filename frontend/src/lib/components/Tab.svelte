1b61e7dc8e1732df6cd9d216fde3e75e6c4eb6cd
Louise Rondia <louiserondia@gmail.com>
2023-05-22 17:53:11 +0200

    ca marche !! les tabs sont responsives

--- frontend/src/lib/components/Tab.svelte
+++ frontend/src/lib/components/Tab.svelte
@@ -4,14 +4,40 @@
 	export let active: boolean;
 </script>
 
-<a href="/" on:click class:active>
+<div class="tab" on:click class:active>
 	<img src={icon} alt={name} />
-	{name}
-</a>
+	<a href="/">
+		{name}
+	</a>
+</div>
 
 <style lang="scss">
-	a {
-		@include tab;
-		margin-right: 0.5rem;
+	.tab {
+		// @include tab;
+		
+		@include tab-contour;
+		@include tab-contour-active;
+		img {
+			@include tab-logo;
+		}
+		
+		background-color: $grey;
+		height: $tab-height;
+		display: flex;
+		padding: 0.2rem 0.8rem;
+		margin-right: 0.25rem;
+		flex-basis: 15rem;
+		flex-shrink: 1;
+		flex-wrap: nowrap;
+		overflow: hidden;
+
+		a {
+			font-size: 1.2rem;
+			text-decoration: none;
+			color: inherit;
+			white-space: nowrap;
+			overflow: hidden;
+			text-overflow: ellipsis;
+		}
 	}
 </style>
