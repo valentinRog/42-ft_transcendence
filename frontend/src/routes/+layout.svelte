01ce00a25cc95816e3682155415b51a064e9d53b
Valentin <valentin.rogister@gmail.com>
2023-05-24 15:49:37 +0200

    solved something by accident

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,9 +1,14 @@
 <script lang="ts">
+	import { token } from '$lib/stores/stores';
+	import { page } from '$app/stores';
 	import '$lib/style/style.scss';
+	console.log($page)
 </script>
 
 <svelte:head>
 	<title>Yo</title>
 </svelte:head>
 
-<slot />
+{#if $token || $page.url.pathname === '/login'}
+	<slot />
+{/if}
