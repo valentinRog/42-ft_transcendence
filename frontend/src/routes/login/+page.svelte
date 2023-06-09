f81032acde839e0262ddf412000333287be2495d
bperraud <74benji@live.fr>
2023-06-09 19:07:35 +0200

    login clean

--- frontend/src/routes/login/+page.svelte
+++ frontend/src/routes/login/+page.svelte
@@ -41,6 +41,8 @@
 			goto('/');
 		}
 	}
+
+	let actionUrl = signup ? `${PUBLIC_BACKEND_URL}/auth/signup` : `${PUBLIC_BACKEND_URL}/auth/signin`;
 </script>
 
 <div id="login">
@@ -51,52 +53,83 @@
 		</div>
 	</div>
 	<div class="content">
-		{#if signup}
-			<form
-				on:submit|preventDefault={handleSubmit}
-				action="{PUBLIC_BACKEND_URL}/auth/signup"
-				method="post"
-				enctype="application/x-www-form-urlencoded"
+		<form
+			on:submit|preventDefault={handleSubmit}
+			action={actionUrl}
+			method="post"
+			enctype="application/x-www-form-urlencoded"
 			>
-				<label for="username">Username</label>
-				<input type="text" id="username" name="username" placeholder="username" />
-				<label for="login">Login</label>
-				<input type="text" id="login" name="login" placeholder="login" />
-				<label for="password">Password</label>
-				<input type="password" id="password" name="password" placeholder="password" />
-				<input type="submit" value="Sign Up" />
-			</form>
-		{:else}
-			<form
-				on:submit|preventDefault={handleSubmit}
-				action="{PUBLIC_BACKEND_URL}/auth/signin"
-				method="post"
-				enctype="application/x-www-form-urlencoded"
-			>
-				<label for="login">Login</label>
-				<input type="text" id="login" name="login" placeholder="login" />
-				<label for="password">Password</label>
-				<input type="password" id="password" name="password" placeholder="password" />
-				<label for="twoFactor">2FA</label>
-				<input type="text" id="twoFactor" name="twoFactor" placeholder="2FA" />
-				<input type="submit" value="Signin" />
-			</form>
-		{/if}
-		{#if errorMessage}
-			<p><strong>{errorMessage}</strong></p>
-		{/if}
-		<button
-			on:click={() => {
-				signup = !signup;
-				errorMessage = null;
-			}}>{signup ? 'I have an account' : 'Create an account'}</button
-		>
+			<div class="form-group">
+				<div class="row-icon"><img src="/user.png"></div>
+				<label for="login">Login:</label>
+				<input type="text" id="login" name="login"/>
+			</div>
+			<div class="form-group">
+				<div class="row-icon"><img src="/keys-3.png"></div>
+				<label for="password">Password:</label>
+				<input type="password" id="password" name="password"/>
+			</div>
+			{#if !signup}
+			<div class="form-group">
+				<div class="row-icon"><img src="/padlock.png"></div>
+				<label for="twoFactor">2FA:</label>
+				<input type="text" id="twoFactor" name="twoFactor"/>
+				</div>
+			{:else}
+			<div class="form-group">
+				<div class="row-icon"><img src="/agent.png"></div>
+				<label for="username">Username:</label>
+				<input type="text" id="username" name="username"/>
+				</div>
+			{/if}
+			<div class="button-container">
+				<button
+				on:click={() => {
+					signup = !signup;
+				}}>{signup ? 'I have an account' : 'Create an account'}</button>
+				<button type="submit">{#if signup}Sign Up{:else}Sign In{/if}</button>
+			</div>
+		</form>
 		<a href="{PUBLIC_BACKEND_URL}/auth/42login">login with 42</a>
+
+
 	</div>
 </div>
 
 <style lang="scss">
+
 	div#login {
+
+		form {
+			display: flex;
+			flex-direction: column; /* Align form-rows vertically */
+			align-items: left; /* Vertically align form-row elements */
+			gap: 0.5rem;
+			padding: 1rem;
+			width: 25rem;
+			margin-top: 1rem;
+
+			.row-icon img {
+				margin-right: 10px;
+				width: 25px;
+ 			    height: 25px;
+			}
+
+			label {
+				width: 7rem;
+			}
+		}
+
+		.form-group {
+			display: flex;
+			align-items: center;
+		}
+
+		input {
+			@include form-95;
+  			margin-left: 10px; /* Add a gap between label and input field */
+		}
+
 		@include tab-contour;
 		background-color: $grey;
 		display: block;
@@ -116,19 +149,19 @@
 			}
 		}
 
-		form {
+		.button-container {
 			display: flex;
-			flex-direction: column;
+			justify-content: center;
+			align-items: center;
 			gap: 0.5rem;
-			padding: 1rem;
-			width: 20rem;
 			margin-top: 1rem;
+		}
 
-			input[type='submit'] {
-				width: fit-content;
-				margin-top: 1rem;
-				align-self: flex-end;
-			}
+		.button-container button {
+			font-size: inherit;
+			padding: 0.3rem 1rem; /* Adjust the padding as desired */
+			width: 10rem; /* Adjust the width as desired */
+
 		}
 
 		div.content {
@@ -143,5 +176,6 @@
 				text-align: center;
 			}
 		}
+
 	}
 </style>
