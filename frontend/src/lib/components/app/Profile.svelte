52e31b9f6cecfa27cbbd6564a8878f9c89330a40
Valentin Rogister <valentin.rogister@gmail.com>
2023-06-14 22:17:52 +0200

    fixed my mess

--- frontend/src/lib/components/app/Profile.svelte
+++ frontend/src/lib/components/app/Profile.svelte
@@ -2,7 +2,12 @@
 	import ProfileInfo from './ProfileInfo.svelte';
 
 	let activeTabIndex = 0;
-	const tabTitle = ['Information', 'Statistics', 'Match history'];
+	const tabTitle = [
+		"Information",
+		"Statistics",
+		"Match history",
+	];
+
 </script>
 
 <div class="window-body">
@@ -12,15 +17,6 @@
 		  <a>{content}</a>
 		</li>
 	  {/each}
-		{#each tabTitle as content, index}
-			<li
-				role="tab"
-				aria-selected={index === activeTabIndex}
-				on:click={() => (activeTabIndex = index)}
-			>
-				<a href="#tabs">{content}</a>
-			</li>
-		{/each}
 	</menu>
 
 	<div class="window" role="tabpanel">
@@ -35,20 +31,11 @@
 			<p class="tab-content">{"stat"}</p>
 		  {/if}
 	  </div>
-		<div class="window-body">
-			{#if activeTabIndex === 0}
-				<div class="window-body">
-					<ProfileInfo />
-				</div>
-			{:else if activeTabIndex === 1}
-				<p class="tab-content">{'stat'}</p>
-			{:else if activeTabIndex === 1}
-				<p class="tab-content">{'stat'}</p>
-			{/if}
-		</div>
 	</div>
-</div>
+  </div>
 
 <style lang="scss">
+
 	@include onglet;
+
 </style>
