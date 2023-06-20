6d5b2a5ee23aaca72878ad3daf01fc1c170baf45
Timmy tam Luu <tluu@fu-r5-p2.s19.be>
2023-06-20 17:59:47 +0200

    chat always work

--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -8,6 +8,29 @@ export class PrismaService extends PrismaClient {
     super({ datasources: { db: { url: configService.get('DATABASE_URL') } } });
   }
 
+  async onModuleInit() {
+    await this.$connect();
+    await this.initRoles();
+  }
+
+  async initRoles() {
+    const roles: any = [
+      { name: 'Admin' },
+      { name: 'Moderator' },
+      { name: 'User' },
+    ];
+
+    for (const role of roles) {
+      const existingRole = await this.role.findUnique({
+        where: { name: role.name },
+      });
+
+      if (!existingRole) {
+        await this.role.create({ data: role });
+      }
+    }
+  }
+
   cleanDb() {
     return this.$transaction([this.stat.deleteMany(), this.user.deleteMany()]);
   }
